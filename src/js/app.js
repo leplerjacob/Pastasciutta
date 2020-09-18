12d66c1515c0a77304793d3c5bd15bcafda723d8
jl <jacoblepler91@gmail.com>
2020-09-18 16:01:56 -0400

    center mobile css

--- src/js/app.js
+++ src/js/app.js
@@ -3,16 +3,31 @@ document.querySelector(".toggle").addEventListener("click", (e) => {
 
   overlay.style.display = "grid";
 
-  console.log(overlay.className);
+  e.preventDefault();
+});
+
+document.querySelector(".close-toggle").addEventListener("click", (e) => {
+  const overlay = document.querySelector(".overlay");
+
+  overlay.style.display = "none";
 
   e.preventDefault();
 });
 
-document.querySelector('.close-toggle').addEventListener("click", (e) => {
-    const overlay = document.querySelector(".overlay");
+document.querySelector(".btn-order").addEventListener("click", (e) => {
+  const overlay = document.querySelector(".menu-order");
+
+  overlay.style.display = "grid";
+
+  e.preventDefault();
+});
+
+document.addEventListener("keydown", (e) => {
+  if (e.keyCode === 27) {
+    const overlay = document.querySelector(".menu-order");
 
     overlay.style.display = "none";
+  }
 
-    e.preventDefault();
-})
-
+  e.preventDefault();
+});
